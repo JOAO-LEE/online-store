5ed7ee65cb906232b667efd92397d60079a3e14d
LucAssis <lucassis7@outlook.com>
2022-05-26 15:50:43 -0300

    Adiciona o requisito 05  -v01

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Category from '../components/Category';
-import { getCategories } from '../services/api';
+import Products from '../components/Products';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends React.Component {
   constructor() {
     super();
 
     this.state = {
+      queryToSearch: '',
       categories: [],
+      products: [],
     };
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({ queryToSearch: value });
+  }
+
+  handleClick = async (event) => {
+    event.preventDefault();
+    const { queryToSearch } = this.state;
+    const searchedProducts = await getProductsFromCategoryAndQuery(null, queryToSearch);
+    this.setState({
+      queryToSearch: '',
+      products: searchedProducts.results,
+    });
   }
 
   componentDidMount = async () => {
@@ -20,16 +38,50 @@ class Home extends React.Component {
   }
 
   render() {
-    const { categories } = this.state;
+    const { queryToSearch, categories, products } = this.state;
     return (
       <div>
-        <Link to="/cart" data-testid="shopping-cart-button">
-          Carrinho
-        </Link>
-        <input type="text" />
-        <h3 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h3>
+        <div>
+          <Link to="/cart" data-testid="shopping-cart-button">
+            Carrinho
+          </Link>
+          <input
+            type="text"
+            data-testid="query-input"
+            value={ queryToSearch }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="submit"
+            data-testid="query-button"
+            onClick={ this.handleClick }
+          >
+            Pesquisar
+          </button>
+          { (products.length === 0)
+            ? (
+              <h3 data-testid="home-initial-message">
+                Digite algum termo de pesquisa ou escolha uma categoria.
+              </h3>
+            )
+            : (
+              <h3>
+                Produtos encontrados:
+              </h3>
+            )}
+          <div>
+            { (products.length === 0)
+              ? <p>Nenhum produto foi encontrado</p>
+              : products.map((product) => (
+                <Products
+                  key={ product.id }
+                  productName={ product.title }
+                  productImage={ product.thumbnail }
+                  productPrice={ product.price }
+                />
+              )) }
+          </div>
+        </div>
         <div>
           {categories.map((category) => (
             <Category
