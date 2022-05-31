c8348295a5d0e72a3e8f59a555f9c5902de08afc
Jake Chien <jake_chien@outlook.com>
2022-05-31 18:39:10 -0300

    Requisito 10 :thumbsup:

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,52 +1,23 @@
 import React from 'react';
-import { getProductsFromId } from '../services/api';
+import ProductsOnCart from '../components/ProductsOnCart';
 
 class Cart extends React.Component {
   state = {
     haveItens: false,
     productOnCart: [],
-    itensOnCart: 0,
-    isButtonEnable: true,
   }
 
   componentDidMount() {
     this.getItemFromStorage();
   }
 
-  increaseAndDecreaseQuantity = ({ target }) => {
-    if (target.name === 'addToButton') {
-      this.setState((prevState) => ({
-        itensOnCart: prevState.itensOnCart + 1,
-      }), this.isEnable);
-    }
-    if (target.name === 'removeButton') {
-      this.setState((prevState) => ({
-        itensOnCart: prevState.itensOnCart - 1,
-      }), this.isEnable);
-    }
-  }
-
-  isEnable = () => {
-    const { itensOnCart } = this.state;
-    if (itensOnCart <= 1) {
-      this.setState({ isButtonEnable: true });
-    } else {
-      this.setState({ isButtonEnable: false });
-    }
-  }
-
   getItemFromStorage = async () => {
     if (localStorage.getItem('idProduct') !== null) {
-      const id = localStorage.getItem('idProduct');
-      const arrayOfId = id.split(',');
-      arrayOfId.forEach(async (element) => {
-        const product = await getProductsFromId(element);
-        this.setState((prevState) => ({
-          haveItens: true,
-          productOnCart: [...prevState.productOnCart, product],
-        }));
+      const product = JSON.parse(localStorage.getItem('idProduct'));
+      this.setState({
+        haveItens: true,
+        productOnCart: product,
       });
-      this.setState((prevState) => ({ itensOnCart: prevState.itensOnCart + 1 }));
     } else {
       this.setState({ haveItens: false });
     }
@@ -55,8 +26,6 @@ class Cart extends React.Component {
   render() {
     const { haveItens,
       productOnCart,
-      itensOnCart,
-      isButtonEnable,
     } = this.state;
     if (!haveItens) {
       return (
@@ -68,27 +37,12 @@ class Cart extends React.Component {
     return (
       <div>
         {productOnCart.map((product) => (
-          <>
-            <p data-testid="shopping-cart-product-name">{product.title}</p>
-            <img src={ product.thumbnail } alt={ product.title } />
-            <p>{product.price}</p>
-            <p data-testid="shopping-cart-product-quantity">{itensOnCart}</p>
-            <input
-              disabled={ isButtonEnable }
-              name="removeButton"
-              type="button"
-              value="-"
-              onClick={ this.increaseAndDecreaseQuantity }
-              data-testid="product-decrease-quantity"
-            />
-            <input
-              name="addToButton"
-              type="button"
-              value="+"
-              onClick={ this.increaseAndDecreaseQuantity }
-              data-testid="product-increase-quantity"
-            />
-          </>
+          <ProductsOnCart
+            key={ product.id }
+            productTitle={ product.title }
+            productThumbnail={ product.thumbnail }
+            productPrice={ product.price }
+          />
         ))}
       </div>
     );
